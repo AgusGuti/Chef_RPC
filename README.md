e4fdd6227d2c29a890a2b8d2aa3dc3100c78d599
sanchezantelo <sofiasa@live.com.ar>
2023-09-02 21:25:16 +0000

    cliente-servidor python

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Chef_RPC
+
+SERVIDOR JAVA RPC
+
+sdk install java 20.0.2-amzn
+
+cd app-java
+mvn clean install -DskipTests
+mvn spring-boot:run
+
+CLIENTE/SERVIDOR PYTHON 
+
+cd app-python
+pip install -r requirements.txt
+python -m grpc_tools.protoc -I../app-java/src/main/proto --python_out=. --pyi_out=. --grpc_python_out=. ../app-java/src/main/proto/account.proto
+flask run
--- app-python/app.py
+++ app-python/app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 import grpc
 import account_pb2
 import account_pb2_grpc
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def account():
-    print("Will try to greet world ...")
-    with grpc.insecure_channel("localhost:9090") as channel:
+
+@app.route("/findAll")
+def findAll():
+    app.logger.info("/findAll")
+    with grpc.insecure_channel(os.getenv("SERVER-JAVA-RPC")) as channel:
         stub = account_pb2_grpc.AccountsServiceStub(channel)
         response = stub.FindAll(account_pb2.Accounts())
-    print("Greeter client received: " + response.message)    
-    return "<p>Hello, World!</p>"
+    print("Greeter client received: " + str(response))    
+    return str(response)
+
+@app.route("/addAccount",methods = ['GET'])
+def findByNumber():
+    app.logger.info("/addAccount",request.args['number'])
+    with grpc.insecure_channel(os.getenv("SERVER-JAVA-RPC")) as channel:
+        stub = account_pb2_grpc.AccountsServiceStub(channel)
+        response = stub.AddAccount(account_pb2.Account(number="888888",customer_id=2))
+    print("Greeter client received: " + str(response))    
+    return str(response)
