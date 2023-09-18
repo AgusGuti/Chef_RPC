2d3ff10684dc66d93840a7c841955c3dec3c075e
sanchezantelo <sofiasa@live.com.ar>
2023-09-18 10:33:59 -0300

    se guardan archivos proto en carpeta

--- app-python/app/proto/receta_pb2_grpc.py
+++ app-python/app/proto/receta_pb2_grpc.py
@@ -3,7 +3,9 @@
 import grpc
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-import receta_pb2 as receta__pb2
+from . import categoria_pb2 as categoria__pb2
+from . import ingrediente_pb2 as ingrediente__pb2
+from . import receta_pb2 as receta__pb2
 
 
 class RecetasServiceStub(object):
