ca32fb5fea51a2b12167e90dbba73722d6137613
AgusGuti <ruso92_04@hotmail.com>
2023-09-28 21:50:36 -0300

    Evita cargar 2 veces el Seguido

--- app-java/src/main/java/com/chefencasa/app/repository/SeguidoRepository.java
+++ app-java/src/main/java/com/chefencasa/app/repository/SeguidoRepository.java
@@ -9,11 +9,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.chefencasa.app.entities.Seguido;
+import com.chefencasa.app.entities.User;
 
 @Repository("seguidoRepository")
 public interface SeguidoRepository extends JpaRepository<Seguido, Serializable> {
 
     @Query("SELECT DISTINCT s FROM Seguido s WHERE s.user.id = :userId")
 	public abstract List<Seguido> findAllSeguidosByUserId(@Param("userId") int userId);
+
+    @Query("SELECT s FROM Seguido s WHERE (s.user.id LIKE :userId AND s.seguido.id LIKE :seguidoId)")
+	public abstract Seguido validarCredenciales(@Param("userId") int userId, @Param("seguidoId") int seguidoId);
+
 
 }
