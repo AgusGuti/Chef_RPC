e20a563ecd15e051bc3702ff13e966086d94e949
sanchezantelo <sofiasa@live.com.ar>
2023-10-19 19:24:56 -0300

    Estructura, agrega dependencia pymysql en apirest

--- app-rest-carga-masiva/app.py
+++ app-rest-carga-masiva/app.py
@@ -438,4 +438,4 @@ def guardar_como_receta(borrador_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001,debug=True)
