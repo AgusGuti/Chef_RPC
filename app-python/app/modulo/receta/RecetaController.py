eae1f94e76ea4ff896b5aec9da3eda15cbbe3bdf
AgusGuti <ruso92_04@hotmail.com>
2023-10-21 13:52:51 -0300

    vista denuncia

--- app-python/app/modulo/receta/RecetaController.py
+++ app-python/app/modulo/receta/RecetaController.py
@@ -173,8 +173,11 @@ def findAll():
         stub = RecetasServiceStub(channel)
         response = stub.FindAll(Receta()) 
         recetas = response.receta 
+
+    motivos = clientDenuncias.service.getMotivos()
+
     print("Greeter client received: " + str(response))    
-    return render_template('storyline.html', recetas=recetas,usuario_autenticado=user_id)
+    return render_template('storyline.html', recetas=recetas,usuario_autenticado=user_id, motivos= motivos)
 
 
 @receta_blueprint.route("/receta/findById/<int:id>",methods = ['GET'])
@@ -222,7 +225,6 @@ def findDenunciasAbiertas():
     
     denuncias_abiertas = clientDenuncias.service.getUnresolved()
     motivos = clientDenuncias.service.getMotivos()
-    recetasId = clientDenuncias.service.getRecetasIds()
 
     denuncia_recetas = []
 
@@ -255,7 +257,6 @@ def findDenunciasAbiertas():
         flash('Error al intentar traer Motivos','danger')
         return redirect('/storyline')
     else:
-        #return render_template('denuncias.html', denuncias_abiertas=denuncias_abiertas, motivos=motivos, recetas_denunciadas= recetas_denunciadas)
         return render_template('denuncias.html', denuncia_recetas= denuncia_recetas, motivos=motivos)
     
 
@@ -266,32 +267,62 @@ def addDenuncia():
     recetaId = int(request.form["receta_id"])
     motivoId = int(request.form["motivo_id"])
     
+    with grpc.insecure_channel(os.getenv("SERVER-JAVA-RPC")) as channel:
+            stub = RecetasServiceStub(channel)
+            receta_denunciada = stub.FindById(Receta(idReceta= recetaId))
+
     
     if not isinstance(session['user_id'], int) and not isinstance(recetaId, int) and not isinstance(motivoId, int):
-        flash('Error al intentar guardar Denuncia','danger')
+        flash(f'Error al intentar guardar Denuncia de receta "{receta_denunciada.tituloReceta}"', 'danger')
         return redirect('/storyline')
     else:
 
         clientDenuncias.service.addDenuncia(user_id=session['user_id'], receta_id= recetaId, motivo_id= motivoId)
 
-        flash('Receta denunciada!','message')
+        flash(f'Receta "{receta_denunciada.tituloReceta}" denunciada!', 'message')
         return redirect('/storyline')
     
-    
+
     
 @receta_blueprint.route("/resolverDenuncia",methods=['POST'])
 def resolverDenuncia():
     logger.info("/resolverDenuncia")
     
     denuncia_id = request.form["denuncia_id"]
-    
+    receta_id = request.form["receta_id"]
+    flg_eliminar = request.form["flg_eliminar"]
 
+    logger.info("Resolviendo Denuncia: %s", denuncia_id)
+    logger.info("Receta Denunciada: %s", receta_id)
+    logger.info("FLG Denuncia: %s", flg_eliminar)
+
+    
     if not isinstance(denuncia_id, int):
         flash('Error al intentar resolver Denuncia','danger')
-        return redirect('/storyline')
+        return redirect('/denuncias')
+    
     else:
+
+        if flg_eliminar == "1":
+            with grpc.insecure_channel(os.getenv("SERVER-JAVA-RPC")) as channel:
+                stub = RecetasServiceStub(channel)
+                stub.DeleteReceta(Receta(idReceta= receta_id))
 
         clientDenuncias.service.resolverDenuncia(id=denuncia_id)
 
         flash('Receta resuelta!','message')
+        return redirect('/denuncias')
+    
+    
+
+@receta_blueprint.route("/getMotivos",methods=['GET'])
+def getMotivos():
+    logger.info("/getMotivos")
+    
+    motivos = clientDenuncias.service.getMotivos()    
+
+    if motivos=="{}":
+        flash('Error al intentar traer Motivos','danger')
         return redirect('/storyline')
+    else:
+        return motivos
